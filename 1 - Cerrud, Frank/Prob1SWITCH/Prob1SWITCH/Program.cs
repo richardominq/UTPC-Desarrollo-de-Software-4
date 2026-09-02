8245c0f7dd569f32de98bf724057e91880bad1a6
frankcerrud26 <frankcerrud12@gmail.com>
2026-09-02 18:14:43 -0500

    8 Programas Frank Cerrud

---
+++ 1 - Cerrud, Frank/Prob1SWITCH/Prob1SWITCH/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("Bienvenido a la tienda.");
+
+Console.WriteLine("Ingrese el monto de su compra.");
+string entradamonto = Console.ReadLine();
+float monto = float.Parse(entradamonto);
+
+Console.WriteLine("¿Qué tipo de usuario es?\n" +
+    "1. Regular\n" +
+    "2. Frecuente\n" +
+    "3. VIP");
+string entradatipo = Console.ReadLine();
+int tipo = int.Parse(entradatipo); // Usamos int para que el switch funcione perfecto
+
+double descuento = 0.15;
+double montofinal;
+
+switch (tipo)
+{
+    case 1:
+        Console.WriteLine("No recibe descuento. El monto original que deberá pagar es: $" + monto);
+        break;
+
+    case 2:
+        if (monto > 100)
+        {
+            montofinal = monto - (monto * descuento);
+            Console.WriteLine("Descuento Aplicado. El monto final es: $" + montofinal);
+        }
+        else
+        {
+            Console.WriteLine("No recibe descuento (monto menor o igual a $100). Monto Final: $" + monto);
+        }
+        break;
+    case 3:
+        if (monto > 100)
+        {
+            montofinal = monto - (monto * descuento);
+            Console.WriteLine("Descuento Aplicado. El monto final es: $" + montofinal);
+        }
+        else
+        {
+            Console.WriteLine("No recibe descuento (monto menor o igual a $100). Monto Final: $" + monto);
+        }
+        break;
+
+    default:
+        Console.WriteLine("Opcion invalida");
+        break;
+}
+
+Console.WriteLine("Gracias por usar la tienda");
---
+++ 1 - Cerrud, Frank/Prob3IF/Prob3IF/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Bienvenido al Sistema de Entrada para Evento");
+
+Console.WriteLine("Ingrese la edad de la persona");
+string entradaedad = Console.ReadLine();
+byte edad = byte.Parse(entradaedad);
+
+Console.WriteLine("Ingrese la cantidad de boletos");
+string entradacantidad = Console.ReadLine();
+int cantidad = int.Parse(entradacantidad);
+
+Console.WriteLine("Ingrese el tipo de entrada\n" +
+    "1. General\n" +
+    "2. VIP\n" +
+    "3. Invitado");
+string entradatipo = Console.ReadLine();
+int tipo = int.Parse(entradatipo);
+
+if (edad >= 18 && cantidad > 0 && (tipo == 1 || tipo == 2 || tipo == 3))
+{
+
+    string nombreTipo = "";
+
+    if (tipo == 1)
+    {
+        nombreTipo = "General";
+    }
+    else if (tipo == 2)
+    {
+        nombreTipo = "VIP";
+    }
+    else if (tipo == 3)
+    {
+        nombreTipo = "Invitado";
+    }
+
+    Console.WriteLine("Entrada autorizada\n" +
+        "Tipo de entrada: " + nombreTipo);
+}
+else
+{
+    Console.WriteLine("Entrada rechazada. No cumple con las condiciones establecidas.");
+}
+
+
