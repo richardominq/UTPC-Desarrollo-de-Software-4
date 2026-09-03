1934c88dbd816cd8f876b75703800d6f51cb52d9
dsMxguel <miguel.sanchez9@utp.ac.pa>
2026-09-02 22:17:06 -0500

    feat:archivos de los programas

---
+++ 19-MiguelSanchez/DescuentoTiendaIf/DescuentoDeTienda.cs
@@ -0,0 +1,39 @@
+﻿/*
+ *Descuento en una tienda: Una tienda desea implementar un pequeño programa para determinar si un cliente puede recibir
+ *un descuento especial. Solicite el monto total de la compra y el tipo de cliente (1 = Regular, 2 = Frecuente, 3 = VIP).
+ *Si la compra es mayor de $100 y el cliente pertenece al tipo 2 o al tipo 3, calcule y muestre el monto final aplicando un 15 % de descuento.
+ *De lo contrario, informe que no recibe descuento y muestre el monto original que deberá pagar.
+ */
+
+using System;
+class DescuentoDeTienda
+{
+    
+    public static void Main (string[] args)
+    {
+
+        double monto;
+        double descuento = 0.15;
+        int cliente;
+        
+        Console.WriteLine("Ingrese el tipo de cliente (1 = Regular, 2 = Frecuente, 3 = VIP):");
+        cliente = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine ("Ingrese el monto total de la compra:");
+        monto = double.Parse(Console.ReadLine());
+
+        if (monto > 100 && (cliente == 2 || cliente == 3))
+        {
+            descuento = monto * descuento;
+            double total = monto - descuento;
+            Console.WriteLine($"si aplico para el descuento del 15%: {descuento}");
+            Console.WriteLine($"total a pagar es : {total}");
+        }
+        else
+        {
+            Console.WriteLine($"No aplico para descuento, su total es {monto}");
+        }
+        
+    }
+}
+
---
+++ 19-MiguelSanchez/EntradaAUnEvento/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Entrada a un evento: Un sistema necesita verificar si una persona cumple las condiciones necesarias para ingresar a un evento.
+ * Solicite la edad, la cantidad de boletos disponibles y el tipo de entrada (1 = General, 2 = VIP, 3 = Invitado).
+ * Si la persona tiene 18 años o más y posee al menos un boleto y seleccionó como tipo de entrada la opción 1, 2 o 3, muestre “Entrada autorizada” junto con el tipo seleccionado.
+ * De lo contrario, muestre “Entrada rechazada” indicando que no cumple con las condiciones establecidas.
+ */
+
+class EntradaAUnEvento
+{
+  public static void Main(string[] args)
+  {
+    int edad;
+    int entrada;
+    int boletos;
+
+    Console.WriteLine("Ingrese su edad:");
+    edad = Convert.ToInt32(Console.ReadLine());
+
+    if (edad < 18)
+    {
+      Console.WriteLine("Es menor de edad, no puede entrar");
+    }
+    else
+    {
+      Console.WriteLine("Ingres la cantidad de boletos:");
+      entrada = Convert.ToInt32(Console.ReadLine());
+      
+      Console.WriteLine("Que tipo de entrada tiene:");
+      Console.WriteLine("1-General");
+      Console.WriteLine("2-VIP");
+      Console.WriteLine("3-Invitado");
+      boletos = Convert.ToInt32(Console.ReadLine());
+
+      if (entrada >= 1 && (boletos == 1 || boletos == 2 || boletos == 3))
+      {
+        Console.WriteLine("Puede Pasar");
+        if (boletos == 1)
+        {
+          Console.WriteLine("Entrada General");
+        }
+        else if (boletos == 2)
+        {
+          Console.WriteLine("entrada vip");
+        }
+        else
+        {
+          Console.WriteLine("entrada como invitado");
+        }
+      }
+      else
+      {
+        Console.WriteLine("No cumple los requisitos\nEntrada rechazada");
+      }
+
+    }
+  }
+}
