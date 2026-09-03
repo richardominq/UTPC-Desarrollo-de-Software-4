d7a995f4e7161e915e4ca40653e5aee4274a6769
frankcerrud26 <frankcerrud12@gmail.com>
2026-09-02 22:30:38 -0500

    4 - Cerrud, Frank

---
+++ 4 - Cerrud, Frank/Prob3SWITCH/Prob3SWITCH/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Bienvenido al Sistema de Entrada para Evento");
+
+Console.WriteLine("Ingrese la edad de la persona");
+string entradaedad = Console.ReadLine();
+byte edad = byte.Parse(entradaedad);
+
+Console.WriteLine("Ingrese la cantidad de boletos");
+string entradacantidad = Console.ReadLine();
+int cantidad = int.Parse(entradacantidad);
+
+Console.WriteLine("Ingrese el tipo de entrada\n" +
+    "1. General\n" +
+    "2. VIP\n" +
+    "3. Invitado");
+string entradatipo = Console.ReadLine();
+int tipo = int.Parse(entradatipo);
+
+if (edad >= 18 && cantidad > 0 && (tipo == 1 || tipo == 2 || tipo == 3))
+{
+    string nombreTipo = "";
+
+    switch (tipo)
+    {
+        case 1:
+            nombreTipo = "General";
+            break;
+        case 2:
+            nombreTipo = "VIP";
+            break;
+        case 3:
+            nombreTipo = "Invitado";
+            break;
+    }
+
+    Console.WriteLine("Entrada autorizada\n" +
+        "Tipo de entrada: " + nombreTipo);
+}
+else
+{
+    Console.WriteLine("Entrada rechazada. No cumple con las condiciones establecidas.");
+}
